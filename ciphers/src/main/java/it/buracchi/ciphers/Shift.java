3befa0461d8bf7b0bf7ab9e195dc8085f027b650
marco buracchi <ma.buracchi@gmail.com>
2016-12-25 00:14:08 +0100

    Code function tested

--- ciphers/src/main/java/it/buracchi/ciphers/Shift.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Shift.java
@@ -3,7 +3,11 @@ package it.buracchi.ciphers;
 public class Shift {
 
 	public String code(String msg, int shiftPosition) {
-		return msg;
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < msg.length(); i++) {
+			result.append((char) (((msg.charAt(i) - 97 + shiftPosition) % 26) + 97));
+		}
+		return result.toString();
 	}
 
 }
--- ciphers/src/test/java/it/buracchi/ciphers/ShiftTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/ShiftTest.java
@@ -8,16 +8,37 @@ import org.junit.Test;
 public class ShiftTest {
 
 	private Shift shift;
+	private String test;
 
 	@Before
 	public void setup() {
 		shift = new Shift();
+		test = "test";
 	}
 
 	@Test
 	public void testShiftCodingZeroPosition() {
-		String test = "test";
-		assertEquals(test,shift.code(test, 0));
+		assertEquals("test", shift.code(test, 0));
+	}
+
+	@Test
+	public void testShiftCodingOnePosition() {
+		assertEquals("uftu", shift.code(test, 1));
+	}
+
+	@Test
+	public void testShiftCodingTwentySixPosition() {
+		assertEquals("test", shift.code(test, 26));
+	}
+
+	@Test
+	public void testShiftCodingTwentySevenPosition() {
+		assertEquals("uftu", shift.code(test, 27));
+	}
+
+	@Test
+	public void testShiftCodingNegativePosition() {
+		assertEquals("sdrs", shift.code(test, -1));
 	}
 
 }
