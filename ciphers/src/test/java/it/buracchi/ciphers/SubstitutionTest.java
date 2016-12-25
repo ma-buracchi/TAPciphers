a7cc49e8915ba37e71d7a4e57f8e2f5c4aad6482
marco buracchi <ma.buracchi@gmail.com>
2016-12-25 01:39:45 +0100

    Implemented coding zero positions

--- ciphers/src/test/java/it/buracchi/ciphers/SubstitutionTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/SubstitutionTest.java
@@ -2,15 +2,23 @@ package it.buracchi.ciphers;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SubstitutionTest {
 	
-	Substitution sub = new Substitution();
+	private Substitution sub;
+	private String test;
+	
+	@Before
+	public void setup(){
+		sub = new Substitution();
+		test = "test";
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSubstitutionCodingZeroPosition() {
+		assertEquals("test", sub.code(test, 0));
 	}
 
 }
