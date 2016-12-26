96050fb5be56459824d16807ab6a429a2468c0e6
marco buracchi <ma.buracchi@gmail.com>
2016-12-26 17:16:40 +0100

    Inserted input manager in shiftCipher

--- ciphers/src/main/java/it/buracchi/ciphers/Shift.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Shift.java
@@ -2,20 +2,30 @@ package it.buracchi.ciphers;
 
 public class Shift {
 
+	private InputManager im;
+	private String message;
+
 	public String code(String msg, int shiftPosition) {
+		processMessage(msg);
 		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < msg.length(); i++) {
-			result.append((char) (((msg.charAt(i) - 97 + shiftPosition) % 26) + 97));
+		for (int i = 0; i < message.length(); i++) {
+			result.append((char) (((message.charAt(i) - 97 + shiftPosition) % 26) + 97));
 		}
 		return result.toString();
 	}
 
 	public String decode(String msg, int shiftPosition) {
+		processMessage(msg);
 		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < msg.length(); i++) {
-			result.append((char) (((msg.charAt(i) - 97 - shiftPosition + 26) % 26) + 97));
+		for (int i = 0; i < message.length(); i++) {
+			result.append((char) (((message.charAt(i) - 97 - shiftPosition + 26) % 26) + 97));
 		}
 		return result.toString();
+	}
+	
+	private void processMessage(String msg) {
+		im = new InputManager();
+		message = im.process(msg);
 	}
 
 }
