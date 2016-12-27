6878cd8ae7b27c58e6005af86fbb91e365ecee19
marco buracchi <ma.buracchi@gmail.com>
2016-12-27 23:59:54 +0100

    Shift refactoring
    Coding and decoding in one method

--- ciphers/src/main/java/it/buracchi/ciphers/Shift.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Shift.java
@@ -4,24 +4,21 @@ public class Shift {
 
 	private String message;
 
-	public String code(String msg, int shiftPosition) {
+	public String code(String msg, int shiftPosition, boolean coding) {
 		processMessage(msg);
 		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < message.length(); i++) {
-			result.append((char) (((message.charAt(i) - 97 + shiftPosition) % 26) + 97));
+		if (coding) {
+			for (int i = 0; i < message.length(); i++) {
+				result.append((char) (((message.charAt(i) - 97 + shiftPosition) % 26) + 97));
+			}
+		} else {
+			for (int i = 0; i < message.length(); i++) {
+				result.append((char) (((message.charAt(i) - 97 - shiftPosition + 26) % 26) + 97));
+			}
 		}
 		return result.toString();
 	}
 
-	public String decode(String msg, int shiftPosition) {
-		processMessage(msg);
-		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < message.length(); i++) {
-			result.append((char) (((message.charAt(i) - 97 - shiftPosition + 26) % 26) + 97));
-		}
-		return result.toString();
-	}
-	
 	private void processMessage(String msg) {
 		InputManager im = new InputManager();
 		message = im.process(msg);
--- ciphers/src/test/java/it/buracchi/ciphers/ShiftTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/ShiftTest.java
@@ -9,61 +9,65 @@ public class ShiftTest {
 
 	private Shift shift;
 	private String test;
+	boolean coding;
+	boolean decoding;
 
 	@Before
 	public void setup() {
 		shift = new Shift();
 		test = "test";
+		coding = true;
+		decoding = false;
 	}
 
 	@Test
 	public void testShiftCodingZeroPosition() {
-		assertEquals("test", shift.code(test, 0));
+		assertEquals("test", shift.code(test, 0, coding));
 	}
 
 	@Test
 	public void testShiftCodingOnePosition() {
-		assertEquals("uftu", shift.code(test, 1));
+		assertEquals("uftu", shift.code(test, 1, coding));
 	}
 
 	@Test
 	public void testShiftCodingTwentySixPosition() {
-		assertEquals("test", shift.code(test, 26));
+		assertEquals("test", shift.code(test, 26, coding));
 	}
 
 	@Test
 	public void testShiftCodingTwentySevenPosition() {
-		assertEquals("uftu", shift.code(test, 27));
+		assertEquals("uftu", shift.code(test, 27, coding));
 	}
 
 	@Test
 	public void testShiftCodingNegativePosition() {
-		assertEquals("sdrs", shift.code(test, -1));
+		assertEquals("sdrs", shift.code(test, -1, coding));
 	}
 	
 	@Test
 	public void testShiftDecodingZeroPosition() {
-		assertEquals(test, shift.decode("test", 0));
+		assertEquals(test, shift.code("test", 0, decoding));
 	}
 	
 	@Test
 	public void testShiftDecodingOnePosition() {
-		assertEquals(test, shift.decode("uftu", 1));
+		assertEquals(test, shift.code("uftu", 1, decoding));
 	}
 	
 	@Test
 	public void testShiftDecodingTwentySixPosition() {
-		assertEquals(test, shift.decode("test", 26));
+		assertEquals(test, shift.code("test", 26, decoding));
 	}
 	
 	@Test
 	public void testShiftDecodingTwentySevenPosition() {
-		assertEquals(test, shift.decode("uftu", 27));
+		assertEquals(test, shift.code("uftu", 27, decoding));
 	}
 	
 	@Test
 	public void testShiftDecodingNegativePosition() {
-		assertEquals(test, shift.decode("sdrs", -1));
+		assertEquals(test, shift.code("sdrs", -1, decoding));
 	}
 
 }
