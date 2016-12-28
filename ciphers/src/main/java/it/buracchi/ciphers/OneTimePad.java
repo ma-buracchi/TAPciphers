abc7bece430f55d66dcb079e1fb183d504c16521
marco buracchi <ma.buracchi@gmail.com>
2016-12-28 18:03:42 +0100

    OTP key creation

--- ciphers/src/main/java/it/buracchi/ciphers/OneTimePad.java
+++ ciphers/src/main/java/it/buracchi/ciphers/OneTimePad.java
@@ -1,16 +1,29 @@
 package it.buracchi.ciphers;
 
+import java.text.ParseException;
+import java.util.Random;
+
 public class OneTimePad {
 	
 	private String key;
 	private String message;
-
+	private Random generator = new Random();
+	
 	public OneTimePad(String msg) {
 		this.message = msg;
+		this.key = createKey();
 	}
-
+	
 	public String getKey() {
 		return key;
+	}
+
+	private String createKey() {
+		StringBuilder k = new StringBuilder();
+		for(Character c : message.toCharArray()){
+			k.append(generator.nextInt(2));
+		}
+		return k.toString();
 	}
 
 }
--- ciphers/src/test/java/it/buracchi/ciphers/OneTimePadTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/OneTimePadTest.java
@@ -2,21 +2,25 @@ package it.buracchi.ciphers;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class OneTimePadTest {
-	
+
 	private OneTimePad otp;
 
-	@Before
-	public void setUp() {
-		otp = new OneTimePad("test");
+	@Test
+	public void testRandomKeyAutoCreation() {
+		otp = new OneTimePad("testmessage");
+		System.out.println(otp.getKey());
+		assertNotEquals("", otp.getKey());
 	}
 
 	@Test
-	public void testKeyAutoCreation() {
-		otp.getKey();
+	public void testRandomAutoCreatedKeyLength() {
+		String test = "testmessage";
+		otp = new OneTimePad(test);
+		System.out.println(otp.getKey());
+		assert (otp.getKey().length() == test.length());
 	}
 
 }
