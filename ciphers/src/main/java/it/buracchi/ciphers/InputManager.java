f4d1b1288c3f0a1c3fc6cad310088f72b691e26e
marco buracchi <ma.buracchi@gmail.com>
2016-12-28 14:06:12 +0100

    InputManager removed code smell
    Assigned a constant to magic numbers

--- ciphers/src/main/java/it/buracchi/ciphers/InputManager.java
+++ ciphers/src/main/java/it/buracchi/ciphers/InputManager.java
@@ -3,17 +3,21 @@ package it.buracchi.ciphers;
 import java.util.Locale;
 
 public class InputManager {
+	
+	public static final int ALPHABET_LENGTH = 26;
+	public static final int ASCII_a = 65;
+	public static final int ASCII_Z = 122;
 
 	public String process(String msg) {
 		return msg.replaceAll("[^A-Za-z]", "").toLowerCase(Locale.ENGLISH);
 	}
 
 	public String checkAlphabet(String alphabet) {
-		if (alphabet.length() != 26) {
+		if (alphabet.length() != ALPHABET_LENGTH) {
 			throw new IllegalArgumentException("New alphabet must be 26 characters long");
 		}
 		for (int i = 0; i < alphabet.length(); i++) {
-			if ((int) (alphabet.charAt(i)) < 65 || (int) (alphabet.charAt(i)) > 122) {
+			if ((int) (alphabet.charAt(i)) < ASCII_a || (int) (alphabet.charAt(i)) > ASCII_Z) {
 				throw new IllegalArgumentException("Letters from A to z are the only available characters");
 			}
 		}
