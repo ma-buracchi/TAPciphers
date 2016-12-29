a9e8ae55a37a2725f37b07287c6eaff866ba6838
marco buracchi <ma.buracchi@gmail.com>
2016-12-29 15:26:04 +0100

    Shift parser mocked

--- ciphers/src/test/java/it/buracchi/ciphers/ShiftTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/ShiftTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 public class ShiftTest {
 
 	private Shift shift;
-	private String test;
 	private Parser parser;
 	
 	@Before
@@ -20,106 +19,91 @@ public class ShiftTest {
 	
 	@Test
 	public void testCreation() {
-		test = "test";
 		assertEquals("", shift.getMessage());
 	}
 
 	@Test
 	public void testShiftCodingZeroPosition() {
-		test = "test";
-		when(parser.process(test)).thenReturn("test");
-		assertEquals(test, shift.code(test, 0));
+		when(parser.process("test")).thenReturn("test");
+		assertEquals("test", shift.code("test", 0));
 	}
 
 	@Test
 	public void testShiftCodingOnePosition() {
-		test = "test";
-		when(parser.process(test)).thenReturn("test");
-		assertEquals("uftu", shift.code(test, 1));
+		when(parser.process("test")).thenReturn("test");
+		assertEquals("uftu", shift.code("test", 1));
 	}
 
 	@Test
 	public void testShiftCodingTwentySixPosition() {
-		test = "test";
-		when(parser.process(test)).thenReturn("test");
-		assertEquals("test", shift.code(test, 26));
+		when(parser.process("test")).thenReturn("test");
+		assertEquals("test", shift.code("test", 26));
 	}
 
 	@Test
 	public void testShiftCodingTwentySevenPosition() {
-		test = "uftu";
-		when(parser.process(test)).thenReturn("uftu");
-		assertEquals(test, shift.code(test, 27));
+		when(parser.process("test")).thenReturn("test");
+		assertEquals("uftu", shift.code("test", 27));
 	}
 
 	@Test
 	public void testShiftCodingNegativePosition() {
-		test = "sdrs";
-		when(parser.process("sdrs")).thenReturn("sdrs");
-		assertEquals(test, shift.code(test, -1));
+		when(parser.process("uftu")).thenReturn("uftu");
+		assertEquals("test", shift.code("uftu", -1));
 	}
 	
 	@Test
 	public void testShiftCodingEmptyString() {
-		test = "";
-		when(parser.process(test)).thenReturn("");
-		assertEquals(test, shift.code(test, 1));
+		when(parser.process("")).thenReturn("");
+		assertEquals("", shift.code("", 1));
 	}
 	
 	@Test
 	public void testShiftCodingStrangeString() {
-		test = "T3es T1!";
-		when(parser.process(test)).thenReturn("test");
-		assertEquals("test", shift.code(test, 0));
+		when(parser.process("T3es T1!")).thenReturn("test");
+		assertEquals("test", shift.code("T3es T1!", 0));
 	}
 	
 	@Test
 	public void testShiftDecodingZeroPosition() {
-		test = "test";
-		when(parser.process(test)).thenReturn("test");
-		assertEquals(test, shift.decode("test", 0));
+		when(parser.process("test")).thenReturn("test");
+		assertEquals("test", shift.decode("test", 0));
 	}
 	
 	@Test
 	public void testShiftDecodingOnePosition() {
-		test = "test";
 		when(parser.process("uftu")).thenReturn("uftu");
-		assertEquals(test, shift.decode("uftu", 1));
+		assertEquals("test", shift.decode("uftu", 1));
 	}
 	
 	@Test
 	public void testShiftDecodingTwentySixPosition() {
-		test = "test";
 		when(parser.process("test")).thenReturn("test");
-		assertEquals(test, shift.decode("test", 26));
+		assertEquals("test", shift.decode("test", 26));
 	}
 	
 	@Test
 	public void testShiftDecodingTwentySevenPosition() {
-		test = "test";
 		when(parser.process("uftu")).thenReturn("uftu");
-		assertEquals(test, shift.decode("uftu", 27));
+		assertEquals("test", shift.decode("uftu", 27));
 	}
 	
 	@Test
 	public void testShiftDecodingNegativePosition() {
-		test = "test";
 		when(parser.process("sdrs")).thenReturn("sdrs");
-		assertEquals(test, shift.decode("sdrs", -1));
+		assertEquals("test", shift.decode("sdrs", -1));
 	}
 	
 	@Test
 	public void testShiftDecodingEmptyString() {
-		test = "";
 		when(parser.process("")).thenReturn("");
-		assertEquals(test, shift.code("", 1));
+		assertEquals("", shift.code("", 1));
 	}
 	
 	@Test
 	public void testShiftDecodingStrangeString() {
-		test = "T3es T1!";
 		when(parser.process("T3es T1!")).thenReturn("test");
-		assertEquals("test", shift.code(test, 0));
+		assertEquals("test", shift.code("T3es T1!", 0));
 	}
 
 }
