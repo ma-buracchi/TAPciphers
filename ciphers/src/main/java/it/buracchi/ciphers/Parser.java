379196d4821c7b126474498ee8e24db643e48707
marco buracchi <ma.buracchi@gmail.com>
2016-12-29 15:33:56 +0100

    Parser removed code smells
    Removed visibility modifier in Parser interface

--- ciphers/src/main/java/it/buracchi/ciphers/Parser.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Parser.java
@@ -2,7 +2,7 @@ package it.buracchi.ciphers;
 
 public interface Parser {
 	
-	public String process(String msg);
-	public String checkAlphabet(String alphabet);
+	String process(String msg);
+	String checkAlphabet(String alphabet);
 
 }
