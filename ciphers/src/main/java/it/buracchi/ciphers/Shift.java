68c07de0aa6f8d2f23474b23d83aa1947ca00a88
marco buracchi <ma.buracchi@gmail.com>
2017-01-02 12:42:03 +0100

    Shift refactoring

--- ciphers/src/main/java/it/buracchi/ciphers/Shift.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Shift.java
@@ -2,43 +2,31 @@ package it.buracchi.ciphers;
 
 public class Shift {
 
-	private String message;
 	private Parser im;
 	private static final int ASCII_OFFSET = 97;
 	private static final int ALPHABET_LENGTH = 26;
 
 	public Shift(Parser parser) {
 		this.im = parser;
-		this.message = "";
-	}
-	
-	public String getMessage() {
-		return message;
 	}
 
-	public String code(String msg, int shiftPosition) {
-		processMessage(msg);
+	public String coding(String msg, int shiftPosition) {
+		String message = im.process(msg);
 		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < message.length(); i++) {
-			result.append(
-					(char) (((message.charAt(i) - ASCII_OFFSET + shiftPosition) % ALPHABET_LENGTH) + ASCII_OFFSET));
+		for (Character c : message.toCharArray()) {
+			result.append((char) (((c - ASCII_OFFSET + shiftPosition) % ALPHABET_LENGTH) + ASCII_OFFSET));
 		}
 		return result.toString();
 	}
 
-	public String decode(String msg, int shiftPosition) {
-		processMessage(msg);
+	public String decoding(String msg, int shiftPosition) {
+		String message = im.process(msg);
 		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < message.length(); i++) {
+		for (Character c : message.toCharArray()) {
 			result.append(
-					(char) (((message.charAt(i) - ASCII_OFFSET - shiftPosition + ALPHABET_LENGTH) % ALPHABET_LENGTH)
-							+ ASCII_OFFSET));
+					(char) (((c - ASCII_OFFSET - shiftPosition + ALPHABET_LENGTH) % ALPHABET_LENGTH) + ASCII_OFFSET));
 		}
 		return result.toString();
-	}
-
-	private void processMessage(String msg) {
-		message = im.process(msg);
 	}
 
 }
--- ciphers/src/test/java/it/buracchi/ciphers/ShiftTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/ShiftTest.java
@@ -18,92 +18,75 @@ public class ShiftTest {
 	}
 	
 	@Test
-	public void testCreation() {
-		assertEquals("", shift.getMessage());
-	}
-
-	@Test
 	public void testShiftCodingZeroPosition() {
 		when(parser.process("test")).thenReturn("test");
-		assertEquals("test", shift.code("test", 0));
+		assertEquals("test", shift.coding("test", 0));
 	}
 
 	@Test
 	public void testShiftCodingOnePosition() {
 		when(parser.process("test")).thenReturn("test");
-		assertEquals("uftu", shift.code("test", 1));
+		assertEquals("uftu", shift.coding("test", 1));
 	}
 
 	@Test
 	public void testShiftCodingTwentySixPosition() {
 		when(parser.process("test")).thenReturn("test");
-		assertEquals("test", shift.code("test", 26));
+		assertEquals("test", shift.coding("test", 26));
 	}
 
 	@Test
 	public void testShiftCodingTwentySevenPosition() {
 		when(parser.process("test")).thenReturn("test");
-		assertEquals("uftu", shift.code("test", 27));
+		assertEquals("uftu", shift.coding("test", 27));
 	}
 
 	@Test
 	public void testShiftCodingNegativePosition() {
 		when(parser.process("uftu")).thenReturn("uftu");
-		assertEquals("test", shift.code("uftu", -1));
+		assertEquals("test", shift.coding("uftu", -1));
 	}
 	
 	@Test
 	public void testShiftCodingEmptyString() {
 		when(parser.process("")).thenReturn("");
-		assertEquals("", shift.code("", 1));
-	}
-	
-	@Test
-	public void testShiftCodingStrangeString() {
-		when(parser.process("T3es T1!")).thenReturn("test");
-		assertEquals("test", shift.code("T3es T1!", 0));
+		assertEquals("", shift.coding("", 1));
 	}
 	
 	@Test
 	public void testShiftDecodingZeroPosition() {
 		when(parser.process("test")).thenReturn("test");
-		assertEquals("test", shift.decode("test", 0));
+		assertEquals("test", shift.decoding("test", 0));
 	}
 	
 	@Test
 	public void testShiftDecodingOnePosition() {
 		when(parser.process("uftu")).thenReturn("uftu");
-		assertEquals("test", shift.decode("uftu", 1));
+		assertEquals("test", shift.decoding("uftu", 1));
 	}
 	
 	@Test
 	public void testShiftDecodingTwentySixPosition() {
 		when(parser.process("test")).thenReturn("test");
-		assertEquals("test", shift.decode("test", 26));
+		assertEquals("test", shift.decoding("test", 26));
 	}
 	
 	@Test
 	public void testShiftDecodingTwentySevenPosition() {
 		when(parser.process("uftu")).thenReturn("uftu");
-		assertEquals("test", shift.decode("uftu", 27));
+		assertEquals("test", shift.decoding("uftu", 27));
 	}
 	
 	@Test
 	public void testShiftDecodingNegativePosition() {
 		when(parser.process("sdrs")).thenReturn("sdrs");
-		assertEquals("test", shift.decode("sdrs", -1));
+		assertEquals("test", shift.decoding("sdrs", -1));
 	}
 	
 	@Test
 	public void testShiftDecodingEmptyString() {
 		when(parser.process("")).thenReturn("");
-		assertEquals("", shift.code("", 1));
+		assertEquals("", shift.coding("", 1));
 	}
 	
-	@Test
-	public void testShiftDecodingStrangeString() {
-		when(parser.process("T3es T1!")).thenReturn("test");
-		assertEquals("test", shift.code("T3es T1!", 0));
-	}
-
 }
