352cf667ef05577ca7f670a0a12bad5b647122ff
marco buracchi <ma.buracchi@gmail.com>
2017-01-02 16:00:12 +0100

    Affine refactoring
    Constant value taken from Constants class

--- ciphers/src/main/java/it/buracchi/ciphers/Affine.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Affine.java
@@ -2,16 +2,12 @@ package it.buracchi.ciphers;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import static it.buracchi.ciphers.Constants.*;
 
 public class Affine {
 
 	private Parser parser;
 	private BiMap<Integer, Integer> invertingTable;
-	private static final int NEGATIVE_AVOIDING = (int) Math.pow(26, 2);
-	private static final int ASCII_OFFSET = 97;
-	private static final int ALPHABET_LENGTH = 26;
-	private static final int[] RELATIVELY_PRIME_TO_26 = { 1, 3, 5, 7, 11, 17, 25 };
-	private static final int[] INVERSE_MOD_26 = { 1, 9, 21, 15, 19, 23, 25 };
 
 	public Affine(Parser p) {
 		parser = p;
@@ -23,7 +19,7 @@ public class Affine {
 		if (invertingTable.containsKey(a)) {
 			StringBuilder res = new StringBuilder();
 			for (Character c : message.toCharArray()) {
-				int letter = (int) (c) - ASCII_OFFSET;
+				int letter = c - ASCII_OFFSET;
 				res.append((char) (((a * letter + b) % ALPHABET_LENGTH) + ASCII_OFFSET));
 			}
 			return res.toString();
@@ -36,7 +32,7 @@ public class Affine {
 		String message = parser.process(msg);
 		StringBuilder res = new StringBuilder();
 		for (Character c : message.toCharArray()) {
-			int letter = (int) (c) - ASCII_OFFSET;
+			int letter = c - ASCII_OFFSET;
 			res.append((char) (((invertingTable.get(a) * (letter - b + NEGATIVE_AVOIDING)) % ALPHABET_LENGTH)
 					+ ASCII_OFFSET));
 		}
