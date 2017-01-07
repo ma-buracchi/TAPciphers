fb49fbff30025077c26176e8c27fe512c0be3c27
marco buracchi <ma.buracchi@gmail.com>
2017-01-07 17:58:46 +0100

    Logging enabled

--- ciphers/src/main/java/it/buracchi/ciphers/Substitution.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Substitution.java
@@ -6,16 +6,18 @@ import static it.buracchi.ciphers.Constants.*;
 
 import java.util.stream.IntStream;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class Substitution {
 
 	private BiMap<Character, Character> convertingTable;
-	private StringBuilder res;
 	private Parser parser;
+	private Logger logger = LogManager.getLogger(Substitution.class);
 
 	public Substitution(Parser p, String newAlphabet) {
 		this.parser = p;
 		setupConvertingTable(parser.checkAlphabet(newAlphabet));
-		res = new StringBuilder();
 	}
 
 	private void setupConvertingTable(String newAlphabet) {
@@ -27,18 +29,24 @@ public class Substitution {
 	}
 
 	public String coding(String msg) {
+		logger.info("Richiesta di cifratura della stringa '" + msg);
 		String message = parser.process(msg);
+		StringBuilder res = new StringBuilder();
 		for (int i = 0; i < message.length(); i++) {
 			res.append(convertingTable.get(message.charAt(i)));
 		}
+		logger.info("Il risultato della cifratura è -----> " + res.toString());
 		return res.toString();
 	}
 
 	public String decoding(String msg) {
+		logger.info("Richiesta di decifratura della stringa '" + msg);
 		String message = parser.process(msg);
+		StringBuilder res = new StringBuilder();
 		for (int i = 0; i < message.length(); i++) {
 			res.append(convertingTable.inverse().get(message.charAt(i)));
 		}
+		logger.info("Il risultato della cifratura è -----> " + res.toString());
 		return res.toString();
 	}
 
