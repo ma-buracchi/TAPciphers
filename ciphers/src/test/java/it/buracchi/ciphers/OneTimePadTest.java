55074b578f0c2a86825d940765fb3e173825228a
marco buracchi <ma.buracchi@gmail.com>
2017-01-10 19:18:02 +0100

    Guava mocked

--- ciphers/src/test/java/it/buracchi/ciphers/OneTimePadTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/OneTimePadTest.java
@@ -15,6 +15,9 @@ public class OneTimePadTest {
 			"101111100", "1011110", "01011", "10100", "0011", "0110", "011110", "101111110", "1110", "1100", "100",
 			"11111", "101110", "10101", "101111101", "011101", "101111111" };
 	private String string_a_BinaryRepresentation = "0100";
+	private String string_t_BinaryRepresentation = "100";
+	private String string_e_BinaryRepresentation = "000";
+	private String string_s_BinaryRepresentation = "1100";
 	private String string_test_BinaryRepresentation = "1000001100100";
 	private String shortKey = "0001";
 	private String longKey = "0001011100101";
@@ -54,33 +57,33 @@ public class OneTimePadTest {
 
 	@Test
 	public void testFromStringToBinary() {
-		when(converter.getValueFromKey('a')).thenReturn("0100");
+		when(converter.getValueFromKey('a')).thenReturn(string_a_BinaryRepresentation);
 		assertEquals(string_a_BinaryRepresentation, otp.fromStringToBinary("a"));
 	}
 
 	@Test
 	public void testFromStringToBinaryLongerString() {
-		when(converter.getValueFromKey('t')).thenReturn("100");
-		when(converter.getValueFromKey('e')).thenReturn("000");
-		when(converter.getValueFromKey('s')).thenReturn("1100");
+		when(converter.getValueFromKey('t')).thenReturn(string_t_BinaryRepresentation);
+		when(converter.getValueFromKey('e')).thenReturn(string_e_BinaryRepresentation);
+		when(converter.getValueFromKey('s')).thenReturn(string_s_BinaryRepresentation);
 		assertEquals(string_test_BinaryRepresentation, otp.fromStringToBinary("test"));
 	}
 
 	@Test
 	public void testFromBinaryToString() {
-		when(converter.containsValue("0100")).thenReturn(true);
-		when(converter.getKeyFromValue("0100")).thenReturn('a');
+		when(converter.containsValue(string_a_BinaryRepresentation)).thenReturn(true);
+		when(converter.getKeyFromValue(string_a_BinaryRepresentation)).thenReturn('a');
 		assertEquals("a", otp.fromBinaryToString(string_a_BinaryRepresentation));
 	}
 
 	@Test
 	public void testFromBinaryToStringLongerString() {
-		when(converter.containsValue("100")).thenReturn(true);
-		when(converter.containsValue("000")).thenReturn(true);
-		when(converter.containsValue("1100")).thenReturn(true);
-		when(converter.getKeyFromValue("100")).thenReturn('t');
-		when(converter.getKeyFromValue("000")).thenReturn('e');
-		when(converter.getKeyFromValue("1100")).thenReturn('s');
+		when(converter.containsValue(string_t_BinaryRepresentation)).thenReturn(true);
+		when(converter.containsValue(string_e_BinaryRepresentation)).thenReturn(true);
+		when(converter.containsValue(string_s_BinaryRepresentation)).thenReturn(true);
+		when(converter.getKeyFromValue(string_t_BinaryRepresentation)).thenReturn('t');
+		when(converter.getKeyFromValue(string_e_BinaryRepresentation)).thenReturn('e');
+		when(converter.getKeyFromValue(string_s_BinaryRepresentation)).thenReturn('s');
 		assertEquals("test", otp.fromBinaryToString(string_test_BinaryRepresentation));
 	}
 
