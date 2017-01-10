f9414de5a5fa4a3212d4f1a795ecfc598cd7447f
marco buracchi <ma.buracchi@gmail.com>
2017-01-10 19:18:11 +0100

    Guava mocked

--- ciphers/src/main/java/it/buracchi/ciphers/Substitution.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Substitution.java
@@ -1,7 +1,5 @@
 package it.buracchi.ciphers;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
 import static it.buracchi.ciphers.Constants.*;
 
 import java.util.stream.IntStream;
@@ -12,42 +10,42 @@ import org.apache.log4j.Logger;
 
 public class Substitution {
 
-	private BiMap<Character, Character> convertingTable;
+	private Mapper<Character, Character> convertingTable;
 	private Parser parser;
 	private static final Logger LOGGER = LogManager.getLogger(Substitution.class);
 
-	public Substitution(Parser p, String newAlphabet) {
+	public Substitution(Parser p, Mapper<Character, Character> convertTable, String newAlphabet) {
 		this.parser = p;
+		this.convertingTable = convertTable;
 		setupConvertingTable(parser.checkAlphabet(newAlphabet));
 		BasicConfigurator.resetConfiguration();
 		BasicConfigurator.configure();
 	}
 
 	private void setupConvertingTable(String newAlphabet) {
-		convertingTable = HashBiMap.create();
 		IntStream.range(0, ALPHABET_LENGTH).forEach(n -> {
 			char a = (char) (n + ASCII_OFFSET);
-			convertingTable.put(a, newAlphabet.charAt(n));
+			convertingTable.addEntry(a, newAlphabet.charAt(n));
 		});
 	}
 
 	public String coding(String msg) {
-		LOGGER.info("Richiesta di cifratura della stringa '" + msg);
+		LOGGER.info("Richiesta di cifratura della stringa " + msg);
 		String message = parser.process(msg);
 		StringBuilder res = new StringBuilder();
 		for (int i = 0; i < message.length(); i++) {
-			res.append(convertingTable.get(message.charAt(i)));
+			res.append(convertingTable.getValueFromKey(message.charAt(i)));
 		}
 		LOGGER.info("Il risultato della cifratura è -----> " + res.toString());
 		return res.toString();
 	}
 
 	public String decoding(String msg) {
-		LOGGER.info("Richiesta di decifratura della stringa '" + msg);
+		LOGGER.info("Richiesta di decifratura della stringa " + msg);
 		String message = parser.process(msg);
 		StringBuilder res = new StringBuilder();
 		for (int i = 0; i < message.length(); i++) {
-			res.append(convertingTable.inverse().get(message.charAt(i)));
+			res.append(convertingTable.getKeyFromValue(message.charAt(i)));
 		}
 		LOGGER.info("Il risultato della cifratura è -----> " + res.toString());
 		return res.toString();
--- ciphers/src/test/java/it/buracchi/ciphers/SubstitutionTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/SubstitutionTest.java
@@ -10,25 +10,32 @@ public class SubstitutionTest {
 
 	private Substitution sub;
 	private Parser parser;
+	private Mapper<Character, Character> convertTable;
 	private String newAlphabet;
 
+	@SuppressWarnings("unchecked")
 	@Before
 	public void setup() {
 		parser = mock(Parser.class);
+		convertTable = mock(Mapper.class);
 		newAlphabet = "qwertyuiopasdfghjklzxcvbnm";
 		when(parser.checkAlphabet(newAlphabet)).thenReturn(newAlphabet);
-		sub = new Substitution(parser, newAlphabet);
+		sub = new Substitution(parser, convertTable, newAlphabet);
 	}
 
 	@Test
 	public void testCoding() {
 		when(parser.process("a")).thenReturn("a");
+		when(convertTable.getValueFromKey('a')).thenReturn('q');
 		assertEquals("q", sub.coding("a"));
 	}
-	
+
 	@Test
 	public void testCodingLongerString() {
 		when(parser.process("test")).thenReturn("test");
+		when(convertTable.getValueFromKey('t')).thenReturn('z');
+		when(convertTable.getValueFromKey('e')).thenReturn('t');
+		when(convertTable.getValueFromKey('s')).thenReturn('l');
 		assertEquals("ztlz", sub.coding("test"));
 	}
 
@@ -41,12 +48,16 @@ public class SubstitutionTest {
 	@Test
 	public void testDecoding() {
 		when(parser.process("q")).thenReturn("q");
+		when(convertTable.getKeyFromValue('q')).thenReturn('a');
 		assertEquals("a", sub.decoding("q"));
 	}
-	
+
 	@Test
 	public void testDecodingLongerString() {
 		when(parser.process("ztlz")).thenReturn("ztlz");
+		when(convertTable.getKeyFromValue('z')).thenReturn('t');
+		when(convertTable.getKeyFromValue('t')).thenReturn('e');
+		when(convertTable.getKeyFromValue('l')).thenReturn('s');
 		assertEquals("test", sub.decoding("ztlz"));
 	}
 
@@ -60,21 +71,21 @@ public class SubstitutionTest {
 	public void testNewAlphabetShorterThenTwentySixLetters() {
 		newAlphabet = "qwertyuiopasdfghj";
 		when(parser.checkAlphabet(newAlphabet)).thenThrow(new IllegalArgumentException());
-		sub = new Substitution(parser, newAlphabet);
+		sub = new Substitution(parser, convertTable, newAlphabet);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testNewAlphabetLongerThenTwentySixLetters() {
 		newAlphabet = "qwertyuiopasdfghjklzxcvbnmqwerty";
 		when(parser.checkAlphabet(newAlphabet)).thenThrow(new IllegalArgumentException());
-		sub = new Substitution(parser, newAlphabet);
+		sub = new Substitution(parser, convertTable, newAlphabet);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testNewAlphabetWithIllegalCharacter() {
 		newAlphabet = "qwertyuiopa?dfghjklzxcvbnm";
 		when(parser.checkAlphabet(newAlphabet)).thenThrow(new IllegalArgumentException());
-		sub = new Substitution(parser, newAlphabet);
+		sub = new Substitution(parser, convertTable, newAlphabet);
 	}
 
 }
