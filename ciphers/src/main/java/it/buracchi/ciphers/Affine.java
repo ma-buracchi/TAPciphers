15fabdd851ac64f42e89d2190ccc7c5de7885119
marco buracchi <ma.buracchi@gmail.com>
2017-01-10 12:49:02 +0100

    Guava mocked

--- ciphers/src/main/java/it/buracchi/ciphers/Affine.java
+++ ciphers/src/main/java/it/buracchi/ciphers/Affine.java
@@ -1,24 +1,21 @@
 package it.buracchi.ciphers;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
-import static it.buracchi.ciphers.Constants.*;
-
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import static it.buracchi.ciphers.Constants.*;
 
 public class Affine {
 
 	private Parser parser;
-	private BiMap<Integer, Integer> invertingTable;
+	private Mapper<Integer, Integer> mapper;
 	private static final int[] RELATIVELY_PRIME_TO_26 = { 1, 3, 5, 7, 11, 17, 25 };
 	private static final int[] INVERSE_MOD_26 = { 1, 9, 21, 15, 19, 23, 25 };
 	private static final Logger LOGGER = LogManager.getLogger(Affine.class);
 
-	public Affine(Parser p) {
+	public Affine(Parser p, Mapper<Integer, Integer> m) {
 		parser = p;
-		setInverse();
+		mapper = m;
 		BasicConfigurator.resetConfiguration();
 		BasicConfigurator.configure();
 	}
@@ -26,8 +23,8 @@ public class Affine {
 	public String coding(String msg, int a, int b) {
 		LOGGER.info("richiesta di cifratura della stringa '" + msg + "' con parametri a = " + a + " e b = " + b);
 		String message = parser.process(msg);
-		if (invertingTable.containsKey(a)) {
-			StringBuilder res = new StringBuilder();
+		StringBuilder res = new StringBuilder();
+		if (mapper.containsKey(a)) {
 			for (Character c : message.toCharArray()) {
 				int letter = c - ASCII_OFFSET;
 				res.append((char) (((a * letter + b) % ALPHABET_LENGTH) + ASCII_OFFSET));
@@ -45,18 +42,11 @@ public class Affine {
 		StringBuilder res = new StringBuilder();
 		for (Character c : message.toCharArray()) {
 			int letter = c - ASCII_OFFSET;
-			res.append((char) (((invertingTable.get(a) * (letter - b + NEGATIVE_AVOIDING)) % ALPHABET_LENGTH)
+			res.append((char) (((mapper.getValueFromKey(a) * (letter - b + NEGATIVE_AVOIDING)) % ALPHABET_LENGTH)
 					+ ASCII_OFFSET));
 		}
 		LOGGER.info("risultato della decifratura -----> " + res.toString());
 		return res.toString();
-	}
-
-	private void setInverse() {
-		invertingTable = HashBiMap.create();
-		for (int i = 0; i < RELATIVELY_PRIME_TO_26.length; i++) {
-			invertingTable.put(RELATIVELY_PRIME_TO_26[i], INVERSE_MOD_26[i]);
-		}
 	}
 
 }
--- ciphers/src/test/java/it/buracchi/ciphers/AffineTest.java
+++ ciphers/src/test/java/it/buracchi/ciphers/AffineTest.java
@@ -11,14 +11,21 @@ public class AffineTest {
 	private Affine affine;
 	private Parser parser;
 
+	private Mapper<Integer, Integer> mapper;
+
 	@Before
 	public void setUp() throws Exception {
 		parser = mock(Parser.class);
-		affine = new Affine(parser);
+		mapper = mock(Mapper.class);
+		when(mapper.getValueFromKey(3)).thenReturn(9);
+		when(mapper.containsKey(3)).thenReturn(true);
+		affine = new Affine(parser, mapper);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testCodingWithWrongCoefficient() {
+		when(mapper.getValueFromKey(2))
+				.thenThrow(new IllegalArgumentException("Coefficient 'a' must be choosen between 1,3,5,7,11,17,25"));
 		affine.coding("test", 2, 5);
 	}
 
@@ -27,31 +34,31 @@ public class AffineTest {
 		when(parser.process("a")).thenReturn("a");
 		assertEquals("f", affine.coding("a", 3, 5));
 	}
-	
+
 	@Test
 	public void testCodingLongerString() {
 		when(parser.process("test")).thenReturn("test");
 		assertEquals("krhk", affine.coding("test", 3, 5));
 	}
-	
+
 	@Test
 	public void testCodingEmptyString() {
 		when(parser.process("")).thenReturn("");
 		assertEquals("", affine.coding("", 3, 5));
 	}
-	
+
 	@Test
 	public void testDecoding() {
 		when(parser.process("f")).thenReturn("f");
 		assertEquals("a", affine.decoding("f", 3, 5));
 	}
-	
+
 	@Test
 	public void testDecodingLongerString() {
 		when(parser.process("krhk")).thenReturn("krhk");
 		assertEquals("test", affine.decoding("krhk", 3, 5));
 	}
-	
+
 	@Test
 	public void testDecodingEmptyString() {
 		when(parser.process("")).thenReturn("");
